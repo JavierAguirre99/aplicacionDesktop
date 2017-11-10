cb2378a162abd07c4a44210dbd16cab6b3bfe3f3
Javier <Javier@192.168.1.5>
2017-11-09 22:55:04 -0600

    Ultimos cambios Javier

---
+++ src/DAO/PersonaDAO.java
@@ -0,0 +1,57 @@
+package DAO;
+
+import MODELO.Personas;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PersonaDAO extends DAO {
+
+    public void ingresarPersona(Personas persona) throws Exception {
+         try{
+            this.conectar();
+             System.out.println(persona.getApellido());
+             System.out.println(persona.getFecha_nac());
+            PreparedStatement st= this.getCn().prepareStatement("insert into persona values(?,?,?,?,?,?,?,?)");                        
+            st.setString(1, persona.getNombre());
+            st.setString(2, persona.getApellido());
+            st.setString(3, persona.getDireccion());
+            st.setInt(4, persona.getDpi());
+            st.setInt(5, persona.getTel_movil());
+            st.setInt(6, persona.getNit());
+            st.setString(7, persona.getFecha_nac());
+            st.setInt(8, persona.getTel_casa());
+            st.executeUpdate();
+        }catch(Exception e){
+             System.out.println("Error en el DAO AL ingresar una Persona" +e);
+        }finally{
+            this.cerrar();
+        }
+    }
+     public List<Personas> listar() throws Exception{
+        List<Personas> lista;
+        lista = new ArrayList();
+        ResultSet result;
+        
+        try {
+            this.conectar();
+            PreparedStatement st =   this.getCn().prepareCall("SELECT id_persona, nombre FROM persona");
+            result = st.executeQuery();
+            
+            
+            while(result.next()){
+                Personas pac = new Personas();
+                pac.setId(Integer.parseInt(result.getString("id_persona")));
+                pac.setNombre(result.getString("nombre"));                
+                lista.add(pac);
+            }
+        } catch (Exception e)  {
+            System.out.println("Error en el dao al intentar listar");
+        }finally{
+            this.cerrar();            
+        }
+        return lista;   
+    }
+}
--- src/jaaer/Jaaer.java
+++ src/jaaer/Jaaer.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jaaer;
-
-/**
- *
- * @author Javier
- */
 public class Jaaer {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        formRegistroPersonas registro = new formRegistroPersonas();
+        registro.show();
     }
     
 }
