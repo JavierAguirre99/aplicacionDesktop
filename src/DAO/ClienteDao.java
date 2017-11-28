5e6ae78eedecb5c17c15a2f822fa24cb883f26ad
Javier <Javier@Casa>
2017-11-28 12:02:49 -0600

    Ultimos cambios Javier

--- src/DAO/ClienteDao.java
+++ src/DAO/ClienteDao.java
@@ -15,48 +15,7 @@ public class ClienteDao extends DAO {
     private String sql;
     private PreparedStatement sta;
 
-    public void modficar(cliente cle) {
-        try {
-            this.conectar();
-            sql = "update clientes set direccion =?, telefono=?, email=? where id_cliente = ?";
-            sta = this.getCn().prepareStatement(sql);
-            sta.setString(1, cle.getDireccion());
-            sta.setInt(2, cle.getTelefono());
-            sta.setString(3, cle.getEmail());
-            sta.setInt(4, cle.getIdCliente());
-            sta.executeUpdate();
-        } catch (Exception ex) {
-            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    }
-
-    public List<cliente> listarCliente(cliente cle) {
-        List<cliente> lista = null;
-        ResultSet res;
-
-        try {
-            this.conectar();
-            sql = "select * from clientes where id_cliente=?";
-            sta = this.getCn().prepareStatement(sql);
-            sta.setInt(1, cle.getIdCliente());
-            res = sta.executeQuery();
-            lista = new ArrayList();
-            while (res.next()) {
-                cliente clie = new cliente();
-                clie.setDireccion(res.getString("direccion"));
-                clie.setEmail(res.getString("email"));
-                clie.setTelefono(res.getInt("telefono"));
-                clie.setIdCliente(res.getInt("id_cliente"));
-                sta.executeQuery();
-                lista.add(clie);
-            }
-        } catch (Exception e) {
-            System.out.println("Error en listar cliente" + e);
-        }
-
-        return lista;
-    }
+   
 
     public ArrayList<InnerPersonaCliente> llenarLista() throws Exception {
         ArrayList<InnerPersonaCliente> lista = null;
